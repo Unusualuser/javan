2eaa406f4628925189ebadd20c802df1c2105fc1
Unusualuser <dryxan.2001@mail.ru>
2020-09-13 13:06:12 +0300

    Add

---
+++ Projects/Practice_5/src/MovableCircle.java
@@ -0,0 +1,37 @@
+public class MovableCircle implements Movable {
+    int radius;
+    MovablePoint center;
+
+    public MovableCircle(int radius, MovablePoint center) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    @Override
+    public String toString() {
+        return "MovableCircle{" +
+                "radius=" + radius +
+                ", center=" + center +
+                '}';
+    }
+
+    @Override
+    public void moveDown() {
+        this.center.y -= this.center.ySpeed;
+    }
+
+    @Override
+    public void moveUp() {
+        this.center.y += this.center.ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        this.center.x -= this.center.xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        this.center.x += this.center.xSpeed;
+    }
+}
