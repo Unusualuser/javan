df59c29d4e8636576123621a8f9fb5a411505104
Unusualuser <dryxan.2001@mail.ru>
2020-09-14 14:53:21 +0300

    Update

--- Projects/Main/src/Practice_3/Human.java
+++ Projects/Main/src/Practice_3/Human.java
@@ -1,9 +1,23 @@
 package Practice_3;
 
 public class Human {
+    String name;
+    int height;
+    Hand leftH;
+    Hand rightH;
+    Head head;
+    Leg leftL;
+    Leg rightL;
     protected double weight;
 
-    public Human(double weight) {
+    public Human(String name, int height, Hand leftH, Hand rightH, Head head, Leg leftL, Leg rightL, double weight) {
+        this.name = name;
+        this.height = height;
+        this.leftH = leftH;
+        this.rightH = rightH;
+        this.head = head;
+        this.leftL = leftL;
+        this.rightL = rightL;
         this.weight = weight;
     }
 
@@ -15,10 +29,73 @@ public class Human {
         this.weight = weight;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Hand getLeftH() {
+        return leftH;
+    }
+
+    public void setLeftH(Hand leftH) {
+        this.leftH = leftH;
+    }
+
+    public Hand getRightH() {
+        return rightH;
+    }
+
+    public void setRightH(Hand rightH) {
+        this.rightH = rightH;
+    }
+
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public Leg getLeftL() {
+        return leftL;
+    }
+
+    public void setLeftL(Leg leftL) {
+        this.leftL = leftL;
+    }
+
+    public Leg getRightL() {
+        return rightL;
+    }
+
+    public void setRightL(Leg rightL) {
+        this.rightL = rightL;
+    }
+
     @Override
     public String toString() {
         return "Human{" +
-                "weight=" + getWeight() +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", left_h=" + leftH +
+                ", right_h=" + rightH +
+                ", head=" + head +
+                ", left_l=" + leftL +
+                ", right_l=" + rightL +
+                ", weight=" + weight +
                 '}';
     }
 }
--- Projects/Main/src/Practice_3/HumanTest.java
+++ Projects/Main/src/Practice_3/HumanTest.java
@@ -2,7 +2,12 @@ package Practice_3;
 
 class HumanTest {
     public static void main(String[] args) {
-        Human andrew = new Human(75);
+        Hand leftH = new Hand(5);
+        Hand rightH = new Hand(4);
+        Head head = new Head(true);
+        Leg leftL = new Leg(100);
+        Leg rightL = new Leg(101);
+        Human andrew = new Human("Andrew", 195, leftH, rightH, head, leftL, rightL, 75);
         System.out.println(andrew.toString());
     }
 }
