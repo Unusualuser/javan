e10f690a52afaa71677ccbb32bea0266fa6a1f1a
Unusualuser <dryxan.2001@mail.ru>
2020-10-15 23:01:53 +0300

    Add + update

--- Projects/Main/src/Practice_12/Enum.java
+++ Projects/Main/src/Practice_12/Enum.java
@@ -2,7 +2,10 @@ package Practice_12;
 
 public class Enum {
     public static void main(String[] args) {
-        printLineWithColor("Hi, guys, it`s me", Colors.BLUE);
+        printLineWithColor("Hi", Colors.BLUE);
+        printLineWithColor("guys,", Colors.GREEN);
+        printLineWithColor("it`s", Colors.RED);
+        printLineWithColor("me", Colors.PINK);
     }
     public static void printLineWithColor(String line, Colors color) {
         System.out.println(color.getColor() + line + "\u001B[0m");
